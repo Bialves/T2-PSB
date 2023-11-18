37dbe3858e069892af36405df4315dad1ab6b1fa
Bianca Alves <biancadsalves@gmail.com>
2023-11-18 12:02:35 -0300

    Fix indentation and pointers

--- src/main.c
+++ src/main.c
@@ -2,93 +2,95 @@
 #include <stdint.h>
 #include "../include/mymemory.h"
 
-int input();
+int input(); // Protótipo da função auxiliar
 
 int main()
 {
-	int choice;
-    mymemory_t *mem = NULL; // Var da memória total
+   unsigned int choice;
+   mymemory_t *mem = NULL; // Var da memória total
 
-    /**
-     * Ao executar, primeira ação é inicializar a memória total,
-     * caso ocorra falha, o programa é encerrado
+   /**
+    * Ao executar, primeira ação é inicializar a memória total,
+    * caso ocorra falha o programa é encerrado
     */
-    size_t size;
-    printf("------- WELCOME! -------\n> Memory Init\nEnter a size: ");
-    scanf("%zu", &size);
+   size_t size;
+   printf("------- WELCOME! -------\n> Memory Init\nEnter a size: ");
+   scanf("%zu", &size);
 
-    mem = mymemory_init(size);
-    if (mem == NULL) { // Inicialização falou...
-        printf("ERROR: initialization failure\n");
-        return 1; // Encerra execução
-    }
-    printf("Memory ptr: %p\n\n", mem);
+   mem = mymemory_init(size);
+   // Se a inicialização falou...
+   if (mem == NULL) {
+      printf("ERROR: initialization failure\n");
+      return 1; // Encerra execução
+   }
+   printf("Memory initialization successfully! Ptr: %p\n", mem);
 
-    do {
-        // Imprime menu de opções
-        printf("------- Options -------\n");
-        printf("1. Alloc Block\n");
-        printf("2. Free Alloc\n");
-        printf("3. Display\n");
-        printf("4. Memory Statics\n");
-        printf("5. Compact\n");
-        printf("6. Exit\n");
-        printf("\n> Enter your choice: ");
-        choice = input();
+   do {
+      // Imprime menu de opções
+      printf(
+         "\n------- Options -------\n"
+         "1. Alloc Block\n"
+         "2. Free Alloc\n"
+         "3. Display\n"
+         "4. Memory Statics\n"
+         "5. Exit\n"
+         "\n> Enter your choice: "
+      );
+      choice = input();
 
-        switch (choice) {
-            case 1: {
-                printf("> Alloc Block\n--\n");
-                // Implementar função
-                printf("\nAllocation successfully!\n");
-                break;
-            }
-            case 2: {
-                printf("> Free Alloc\nAddress: ");
-                unsigned long ptr;  
-                scanf("%lx", &ptr);
+      switch (choice) {
+         case 1: {
+            printf("> Alloc Block\n--\n");
+            // Implementar função
+            printf("\nAllocation successfully!\n");
+            break;
+         }
+         case 2: {
+            printf("> Free Alloc\nAddress: ");
+            unsigned long ptr;
+            scanf("%lx", &ptr);
 
-                mymemory_free(mem, &ptr);
-                printf("Free successfully!\n");
-                break;
-            }
-            case 3: {
-                printf("> Display\n");
-                mymemory_display(mem);
-                break;
-            }
-            case 4: {
-                printf("> Memory Statics\n");
-                // Implementar função
-                break;
-            }
-            case 5: {
-                printf("> Compact\n");
-                // Implementar função
-                break;
-            }
-            case 6: {
-                printf("Interrupting...\n");
-                break;
-            }
-            default:
-                printf("ERROR: wrong Input\n");
-        }
-    } while (choice != 6);
+            mymemory_free(mem, (void *)ptr);
+            printf("Free successfully!\n");
+            break;
+         }
+         case 3: {
+            printf("> Display\n");
+            mymemory_display(mem);
+            break;
+         }
+         case 4: {
+            printf("> Memory Statics\n");
+            // Implementar função
+            break;
+         }
+         case 5: {
+            printf(
+               "> Exit\n"
+               "Cleaning up memory... Bye bye\n"
+            );
+            break;
+         }
+         default:
+            printf("ERROR: wrong Input\n");
+      }
+   } while (choice != 5);
 
-    // Antes de encerrar execução, libera a memória total
-    mymemory_cleanup(mem);
-
-    return 0;
+   /**
+    * Antes de encerrar execução, libera a memória
+    * e os blocos alocados nela
+    */
+   mymemory_cleanup(mem);
+   return 0;
 }
 
 /**
  * Função auxiliar que recebe
  * entradas númericas do usuário
-*/
+ */
 int input()
 {
-	unsigned int number;
-	scanf("%d", &number);
-	return (number);
+   unsigned int number;
+   scanf("%d", &number);
+   return (number);
 }
--- src/mymemory.c
+++ src/mymemory.c
@@ -4,84 +4,88 @@
 #include "../include/mymemory.h"
 
 // Implementations
-mymemory_t* mymemory_init(size_t size) 
+mymemory_t *mymemory_init(size_t size)
 {
-    mymemory_t *memory = (mymemory_t*)malloc(sizeof(mymemory_t));
-    // Falha na alocação de memória para mymemory_t
-    if (memory == NULL) {
-        return NULL;
-    }
+   mymemory_t *memory = (mymemory_t *)malloc(sizeof(mymemory_t));
+   // Falha na alocação de memória para mymemory_t
+   if (memory == NULL) {
+      return NULL;
+   }
 
-    memory->pool = malloc(size);
-    // Falha na alocação de memória para o pool
-    if (memory->pool == NULL) {
-        free(memory);
-        return NULL;
-    }
+   memory->pool = malloc(size);
+   // Falha na alocação de memória para o pool
+   if (memory->pool == NULL) {
+      free(memory); // Libera a alocação para mymemory_t
+      return NULL;
+   }
 
-    memory->total_size = size;
-    memory->head = NULL;
-    return memory;
+   memory->total_size = size;
+   memory->head = NULL;
+   return memory;
 }
 
-void* mymemory_alloc(mymemory_t *memory, size_t size);
+void *mymemory_alloc(mymemory_t *memory, size_t size);
 void mymemory_free(mymemory_t *memory, void *ptr)
 {
-    /*
+   /*
     * Necessário realizar um compact antes
     * de percorrer, para evitar cair em gaps
     */
-    allocation_t *current = memory->head; // Começo da lista
-    allocation_t *prev = NULL; // Var auxiliar para trabalhar referências
-    
-    // Percorre a lista em busca do endereço informado
-    while (current != NULL) {
-        // Se encontrou o endereço...
-        if (current->start == ptr) {
-            if (prev != NULL) { // Se prev não é null
-                prev->next = current->next;
-            } else { // Senão, remoção do inicio
-                memory->head = current->next;
-            }
-            
-            free(current); // Por fim, libera o endereço
-            return;
-        }
-        prev = current;
-        current = current->next;
-    }
+   allocation_t *current = memory->head; // Começo da lista
+   allocation_t *prev = NULL; // Var auxiliar para trabalhar referências
+
+   // Percorre a lista em busca do endereço informado
+   while (current != NULL) {
+      // Se encontrou o endereço...
+      if (current->start == ptr)
+      {
+         if (prev != NULL)
+         { // Se prev não é null
+            prev->next = current->next;
+         }
+         else
+         { // Senão, remoção do inicio
+            memory->head = current->next;
+         }
+
+         free(current); // Por fim, libera o endereço
+         return;
+      }
+      // Atualiza as referências para avançar na lista
+      prev = current;
+      current = current->next;
+   }
 }
 
 void mymemory_display(mymemory_t *memory)
 {
-    /*
+   /*
     * Necessário realizar um compact antes
     * de percorrer, para evitar cair em gaps
     */
-    allocation_t *current = memory->head; // Começo da lista
-
-    while (current != NULL) {
-        printf("Start: %p, Size: %zu\n", current->start, current->size);
-        current = current->next;
-    }
+   allocation_t *current = memory->head; // Começo da lista
+   // Enquanto houver blocos alocados...
+   while (current != NULL) {
+      printf("Start: %p, Size: %zu\n", current->start, current->size);
+      current = current->next;
+   }
 }
 
 void mymemory_stats(mymemory_t *memory);
-void mymemory_compact(mymemory_t *memory);
 
 void mymemory_cleanup(mymemory_t *memory)
 {
-    /*
+   /*
     * Necessário realizar um compact antes
     * de limpar, para evitar cair em gaps
     */
 
-    allocation_t *current = memory->head; // Começo da lista
-
-    while (current != NULL) { // Enquanto houver blocos alocados...
-        allocation_t *next = current->next; // next recebe o próximo
-        free(current); // O bloco atual é liberado
-        current = next;
-    }
-    free(memory); // Por fim, a memória total é liberada
+   allocation_t *current = memory->head; // Começo da lista
+   // Enquanto houver blocos alocados...
+   while (current != NULL) {
+      allocation_t *next = current->next; // next recebe o próximo
+      free(current); // O bloco atual é liberado
+      current = next;
+   }
+   free(memory); // Por fim, a memória total é liberada
 }
